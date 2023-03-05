c6267401e88f522d0ec213a44979ce59e7a0079d
Nelson C <n.devpro@protonmail.com>
2023-03-05 00:38:43 -0400

    dev1.0.9: New Features

--- components/NewFeatures.tsx
+++ components/NewFeatures.tsx
@@ -2,10 +2,27 @@
 
 import React from 'react';
 
-const NewFeatures = () => (
-  <div>
-    New Features
-    <p>new features</p>
+import styles from '../styles';
+
+type NewFeaturesType = {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+};
+
+const NewFeatures = ({ imgUrl, title, subtitle }: NewFeaturesType) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <h1 className="mt-[26px] font-bold text-[24px] leaading-[30px] text-white">
+      Title {title}
+    </h1>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]">
+      {subtitle}
+    </p>
   </div>
 );
 
