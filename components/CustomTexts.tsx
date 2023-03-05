8114ecb2f4b8e0f1424303475c819b3189cd5de8
Nelson C <n.devpro@protonmail.com>
2023-03-04 20:29:32 -0400

    dev1.0.5: About Section

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,7 +1,28 @@
 'use client';
 
+import { motion } from 'framer-motion';
 import React from 'react';
 
-export const TypingText = () => <p>Typing Text</p>;
+import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TitleText = () => <h2>Title Text</h2>;
+type TextsProps = {
+  title: string;
+  textStyles: string;
+};
+
+export const TypingText = ({ title, textStyles }: TextsProps) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter) => (
+      <motion.span variants={textVariant2} key={letter}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
+
+export const TitleText = ({ title, textStyles }: TextsProps) => (
+  <h2 className={`${textStyles}`}>{title}</h2>
+);
